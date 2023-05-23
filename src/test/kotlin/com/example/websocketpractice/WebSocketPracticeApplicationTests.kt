2b2c708487cfdd0f48d810c902a1ab6b156a0da2
Jeong Jiun <jeong_jiun@fk.cocone.co.jp>
2023-05-23 11:43:33 +0900

    init

---
+++ src/test/kotlin/com/example/websocketpractice/WebSocketPracticeApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.example.websocketpractice
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class WebSocketPracticeApplicationTests {
+
+    @Test
+    fun contextLoads() {
+    }
+
+}
